7fd94acc94c8680f6cd7ce7871365dc6b309d8e0
Jon Jahren <jj@linux.com>
2015-08-31 12:00:54 +0200

    La til oppgaver til oblig 1

---
+++ Characterprog.java
@@ -0,0 +1,14 @@
+public class Characterprog {
+	
+	public static void main(String[] args) {
+	char a = 'a';
+	char b = 'b';
+	int c = a + b;
+	System.out.println("Unicodeverdien til karakteren c er " + (int)'c');
+	System.out.println("Unicodesymbolet for c er " +  (char)99);
+	System.out.println("Unicodeverdien for variabelen c er " + (char)c);
+	System.out.println("æ = " + (int)'æ');
+	System.out.println("ø = " + (int)'ø');
+	System.out.println("å = " + (int)'å');
+	}
+}
---
+++ Obligteller.java
@@ -0,0 +1,8 @@
+public class Obligteller {
+
+	public static void main(String[] args) {
+		for(int teller = 0; teller < 20; teller++) {
+			System.out.println(teller);
+		}
+	}
+}
